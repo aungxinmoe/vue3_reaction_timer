463b290e6223c0c8a36ffb8771da07ff8df4bae7
aungzinmoe <aungzinmoe30518@gmail.com>
2021-07-26 13:32:04 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>How fast can u catch me?</h1>
+  <button @click="startGame" :disabled="isPlaying">Play</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" @endGame="endGame" />
+  </div>
+ <div v-if="end">
+    <Result :score="score" />
+ </div>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
+export default {
+  name: "App",
+  components: {
+    Block,
+    Result,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: 0,
+      end:false,
+    };
+  },
+  methods: {
+    startGame() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+    },
+    endGame(score) {
+      this.score = score;
+      this.isPlaying = false;
+      this.end=true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Result is {{ score }}ms</p>
+  <div v-if="score < 250">
+    <p>Your too fast</p>
+  </div>
+  <div v-else-if="score > 250 && score < 500">
+    <p>Your Normal</p>
+  </div>
+  <div v-else>
+    <p>Your are to slow.Try Again</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+};
+</script>
+
+<style>
+</style>
